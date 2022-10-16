61b7eddfd0b73b554fa6347cce982a32697416de
unknown <newmankal@live.com>
2022-10-16 17:37:35 -0400

    Added added logout action

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 
 import {Link} from 'react-router-dom'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useLogout } from '../../hooks/useLogout'
 //styles
 import styles from './Navbar.module.css'
+     
 
 const Navbar = () => {
+
+    const{logout} = useLogout()
   return (
     <nav  className={styles.navbar}>
         <ul>
             <li className={styles.title}>myMoney</li>
+            
             <li><Link to='/login'>Login</Link></li>
             <li><Link to='/signup'>Signup</Link></li>
+            <li> <button  className='btn button'
+            onClick={logout}
+            >Logout</button> </li>
         </ul>
 
     </nav>
