204275c19e892ce7b270e4a9f32b8baa0f2f1ca0
unknown <newmankal@live.com>
2022-10-18 19:11:56 -0400

    Added transactionform to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 //styles
 import styles from './Home.module.css'
+import TransactionForm from './TransactionForm'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <section className={styles.content}>
+        content
+      </section>
+      <section className={styles.side} >
+        <TransactionForm/>
+        <ul className="aside">
+          <li></li>
+        </ul>
+      </section>
+
+    </div>
   )
 }
 
