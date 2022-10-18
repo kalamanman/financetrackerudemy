34ae799401ed731a95cc681f2b9723ad43e94b5b
unknown <newmankal@live.com>
2022-10-18 11:10:50 -0400

    Added added routes guards for not logged user

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import {BrowserRouter,Routes,NavLink,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,NavLink,Route,Navigate} from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 import Home from './pages/home/Home'
 import LogIn from './pages/logIn/LogIn'
 import SignUp from './pages/signUp/SignUp'
 
 function App() {
-  
-
+     
+     const {user} = useAuthContext()
   return (
     <div className="App">
         <BrowserRouter>
         <Navbar/>
          <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<LogIn/>} />
-          <Route path='/signup' element={<SignUp/>} />
+           <Route path='/' element={user?<Home/>:<Navigate to ='/login'/>} /> 
+          <Route path='/login' element={!user?<LogIn/>:<Navigate to='/'/>} />
+          <Route path='/signup' element={!user?<SignUp/>:<Navigate to ='/'/>} />
           <Route path='*' element={<><h1> page not found status 404</h1></>}/>
          </Routes>
         </BrowserRouter>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,18 +7,24 @@ import styles from './Navbar.module.css'
      
 
 const Navbar = () => {
-
+         const {user}           = useAuthContext()
     const{logout} = useLogout()
   return (
     <nav  className={styles.navbar}>
         <ul>
             <li className={styles.title}>myMoney</li>
-            
+            { !user ?
+              (<>
             <li><Link to='/login'>Login</Link></li>
             <li><Link to='/signup'>Signup</Link></li>
+            </>):
+            (<>
+              <li>Hello {user.displayName}</li>
             <li> <button  className='btn button'
             onClick={logout}
             >Logout</button> </li>
+            </>)
+       }
         </ul>
 
     </nav>
