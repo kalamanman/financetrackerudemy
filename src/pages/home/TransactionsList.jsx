321554370225dfb07f84203c9d2881b4ff7caf61
unknown <newmankal@live.com>
2022-11-10 13:34:25 -0500

    Added added createdAt to TransactionList

--- src/pages/home/TransactionsList.jsx
+++ src/pages/home/TransactionsList.jsx
@@ -11,7 +11,9 @@ const TransactionsList = ({transactions}) => {
             <li key={transaction.id}>
                
                 <span className={styles.name} >{transaction.name} </span>
-              <span className={styles.date}>{transaction.createdAt.toDate().toDateString()}</span>
+              <span className={styles.date}>
+                {transaction.createdAt.toDate().toDateString()}
+                </span>
                 <span className={styles.amount} >$ {transaction.amount}</span>
             
                 <button onClick={()=>deleteDocument(transaction.id)}>X</button>
